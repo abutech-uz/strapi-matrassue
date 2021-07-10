a0a694068e4bb09680e9252164bfb3c7adae7494
Malika Bakhtiyorova <malikabakhtiyorova@yandex.ru>
2021-07-11 04:41:50 +0500

    Create Strapi starter project

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'c2310118db1c49a3fa138b79e69e8856'),
+    },
+  },
+});
